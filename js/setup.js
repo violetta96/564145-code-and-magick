a709bc6b0cacbc18acb3d02972fb39f19e3db184
Vialanta Sviryd <36696439+violetta96@users.noreply.github.com>
2018-11-22 20:29:23 +0300

    Исправлена опечатка

--- js/setup.js
+++ js/setup.js
@@ -28,7 +28,7 @@ var renderWizard = function (wizard) {
   var wizardElement = similarWizardTemplate.cloneNode(true);
   wizardElement.querySelector('.setup-similar-label').textContent = wizard.name;
   wizardElement.querySelector('.wizard-coat').style.fill = wizard.coatColor;
-  wizardElement.querySelector('.wizard-eyes').style.fill = wizard.coatColor;
+  wizardElement.querySelector('.wizard-eyes').style.fill = wizard.eyesColor;
   return wizardElement;
 };
 
